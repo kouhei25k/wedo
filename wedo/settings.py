264a6c4d97a0331116dbbe70637d18358e47a230
kouhei <kouhei@DESKTOP-2LH624T>
2021-01-09 04:15:05 +0900

    fix

--- wedo/settings.py
+++ wedo/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ASGI
 ASGI_APPLICATION = "wedo.routing.application"
 
