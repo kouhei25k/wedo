59db5e102159fcf409a11112bf66b335ae905d3e
kouhei <kouhei@DESKTOP-2LH624T>
2021-01-09 02:47:49 +0900

    fix asgi

--- wedo/asgi.py
+++ wedo/asgi.py
@@ -2,6 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wedo.settings')
 django.setup()
 application = get_default_application()
